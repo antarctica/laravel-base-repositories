ae1a29116bf70241938b0758c56ea1896be28947
Felix Fennell <felnne@bas.ac.uk>
2015-01-12 12:13:49 +0000

    Adding export function
    This simplifies code for methods (they don't need to figure out how to return the correct data type) and allows this logic to be centralised.
    
    Though this method supports multiple types of input (all converted to an array) it isn't yet possible to 'inject' other types in without replacing this entire method, this will therefore need to be reconsidered.
    
    Methods that return a single result are not currently handled and still export to an array and then call the export function. This is fine since the export function passes arrays straight through. This will be fixed soon.
    
    The delete method will also be updated to ensure it returns a result in the expected format (of an array).

--- src/Repository/BaseRepositoryEloquent.php
+++ src/Repository/BaseRepositoryEloquent.php
@@ -2,6 +2,9 @@
 
 namespace Antarctica\LaravelBaseRepositories\Repository;
 
+use Illuminate\Database\Eloquent\Collection;
+use Antarctica\LaravelBaseExceptions\Exception\InvalidArgumentTypeException;
+
 abstract class BaseRepositoryEloquent implements BaseRepositoryInterface {
 
     protected $model;
@@ -22,33 +25,45 @@ abstract class BaseRepositoryEloquent implements BaseRepositoryInterface {
      */
     public function all(array $options = [])
     {
-        return $this->model->all()->toArray();
+        $resultSet = $this->model->all();
+
+        return $this->export($resultSet);
     }
 
     /**
      * Return a single record/entity specified by an $id
+     *
+     * TODO Handle export within export() method
      *
      * @param string $id
      * @return array
      */
     public function find($id)
     {
-        return $this->model->findOrFail($id)->toArray();
+        $result = $this->model->findOrFail($id)->toArray();
+
+        return $this->export($result);
     }
 
     /**
      * Create new record/entity from $input
+     *
+     * TODO Handle export within export() method
      *
      * @param array $input
      * @return array
      */
     public function create(Array $input)
     {
-        return $this->model->create($input);
+        $result = $this->model->create($input);
+
+        return $this->export($result);
     }
 
     /**
      * Delete record/entity specified by an $id
+     *
+     * TODO: This should be handled by export() (to be wrapped in a proper array etc.)
      *
      * @param $id
      * @return bool
@@ -59,5 +74,48 @@ abstract class BaseRepositoryEloquent implements BaseRepositoryInterface {
         $this->model->find($id)->delete();
 
         return true;
+    }
+
+    private function export($resultSet)
+    {
+        if (is_object($resultSet))
+        {
+            // Exporting depends on what needs exporting (i.e. what class are we exporting)
+            switch (get_class($resultSet))
+            {
+                case 'Illuminate\Database\Eloquent\Collection':
+
+                    return $this->exportEloquentCollection($resultSet);
+                    break;
+            }
+        }
+
+        if (is_array($resultSet))
+        {
+            // Arrays are already in the right format but need to be wrapped in a data key
+
+            return [
+                'data' => $resultSet
+            ];
+        }
+
+        throw new InvalidArgumentTypeException(
+            $argumentName = 'repository_export',
+            $valueOfCorrectArgumentType = [],
+            $argumentValue = $resultSet
+        );
+    }
+
+    /**
+     * Exports Eloquent collections as an array
+     *
+     * @param Collection $resultSet
+     * @return array
+     */
+    private function exportEloquentCollection(Collection $resultSet)
+    {
+        return [
+            'data' => $resultSet->toArray()
+        ];
     }
 }
